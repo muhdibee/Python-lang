9a50453c549922a6a047c36cfc9e03ea34af055e
Muhammad <muhdibee@gmail.com>
2022-12-21 17:07:06 +0100

    Create a class.

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
 print('Hello Word')  # Print a string
+
+# Create a class
+
+
+class Puppy:  # create a class called Puppy
+    # Define variables for the class (Similar to constructor in JavaScript).
+    def __init__(self, name, toy):
+        self.name = name  # Initialize received values.
+        self.toy = toy
+
+
+zion = Puppy('zion', 'lizard')  # Create instance member of class Puppy.
+print(zion.name)
