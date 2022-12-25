9966a5006bcd9ab416e88c7378748c2164ba298c
Muhammad <muhdibee@gmail.com>
2022-12-25 22:42:51 +0100

    Getting the type of variables.

--- fundamentals/index.py
+++ fundamentals/index.py
@@ -1,6 +1,14 @@
-print("Hello")  # Print hello to the screen
+# Print hello to the screen
+print("Hello")
 
 
 # Collecting user input
 name = input("Hi, what's your name? ")
 print("Hello", name)
+
+# Getting the type of variables.
+age = 35
+email_address = 'muhdibee@gmail.com'
+
+print('Variable type for ', email_address, ' is ', type(email_address))
+print('Variable type for ', age, ' is ', type(age))
