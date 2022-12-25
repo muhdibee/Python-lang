9f84f3e06d660cc3b776f4eccc4e54abe1af4436
Muhammad <muhdibee@gmail.com>
2022-12-25 22:55:47 +0100

    Comparison and if else statement

--- fundamentals/index.py
+++ fundamentals/index.py
@@ -12,3 +12,14 @@ email_address = 'muhdibee@gmail.com'
 
 print('Variable type for ', email_address, ' is ', type(email_address))
 print('Variable type for ', age, ' is ', type(age))
+
+# Comparison and if else statement
+print('Hi!')
+name = input('Whats your name? ')
+print('Nice to meet you. ', name)
+response = input('Are you enjoying the course.')
+
+if response == 'Yes':
+    print("That's good to hear!")
+else:
+    print("Oh no! That's sad to hear.")
