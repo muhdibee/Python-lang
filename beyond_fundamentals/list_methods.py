d190b65170e0fa9e24791c4c21069498a81362d6
Muhammad <muhdibee@gmail.com>
2022-12-26 15:50:19 +0100

    Count method for list.

---
+++ beyond_fundamentals/list_methods.py
@@ -0,0 +1,10 @@
+flips = [
+    'heads',
+    'tails',
+    'tails',
+    'heads',
+    'tails',
+]
+
+print(f'Head occurrence: ', flips.count('heads'))
+print(f'tails occurrence: ', flips.count('tails'))
