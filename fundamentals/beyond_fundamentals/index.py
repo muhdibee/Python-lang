f0516dc14ac96ac5832569367e4d61afc3d78d14
Muhammad <muhdibee@gmail.com>
2022-12-26 11:17:00 +0100

    Creating dictionary.

--- fundamentals/beyond_fundamentals/index.py
+++ fundamentals/beyond_fundamentals/index.py
@@ -1,7 +1,15 @@
-# Creating list.
+# Creating list. Also called array in JavaScript
 cities = [
     'Tokyo',
     'Dakar',
     'Mumbai',
     'Buenos Aires',
 ]
+
+# Creating dictionary. Also called object in JavaScript.
+california_symbols = {
+    'bird': 'California quail',
+    'animal': 'Grizzly bear',
+    'flower': 'California poppy',
+    'fruit': 'Avocado',
+}
